d28d7aa42341826bc4e4542cb0e84fd957f71953
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-04-16 14:25:37 +0200

    using library and base files from previous graphical proejctsd for template setup

---
+++ Makefile
@@ -0,0 +1,52 @@
+NAME    := fdf
+CFLAGS  := -Wextra -Wall -Werror -Wunreachable-code -Ofast -g -O0
+LIBFT   := ./libraries/libft
+LIBMLX  := ./libraries/mlx42
+HEADERS := -I ./include -I $(LIBMLX)/include -I ./libraries/get_next_line -I $(LIBFT)
+LIBS    := $(LIBMLX)/build/libmlx42.a $(LIBFT)/libft.a -ldl -L/opt/homebrew/lib -lglfw -pthread -lm
+SRCS    :=	libraries/get_next_line/get_next_line.c \
+			libraries/get_next_line/get_next_line_utils.c \
+			source/main.c \
+			source/plot_line.c \
+			source/load_map.c \
+			source/utils.c \
+			source/plot_map.c \
+			source/init.c \
+			source/scale.c \
+			source/utils2.c \
+			source/ft_xtoi.c \
+			source/colors.c \
+			source/input.c \
+			source/apply.c
+OBJ_DIR := obj
+SRC_DIRS := $(sort $(dir $(SRCS)))
+OBJ_DIRS := $(addprefix $(OBJ_DIR)/,$(SRC_DIRS))
+OBJS    := $(SRCS:%.c=$(OBJ_DIR)/%.o)
+
+all: directories libmlx libft $(NAME)
+
+directories:
+	@mkdir -p $(OBJ_DIRS)
+
+libft:
+	@$(MAKE) -C $(LIBFT)
+
+libmlx:
+	@cmake $(LIBMLX) -B $(LIBMLX)/build && make -C $(LIBMLX)/build -j4
+
+$(OBJ_DIR)/%.o: %.c
+	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS) && printf "Compiling: $(notdir $<)\n"
+
+$(NAME): $(OBJS)
+	@$(CC) $(OBJS) $(LIBS) $(HEADERS) -o $(NAME)
+
+clean:
+	@rm -rf $(OBJ_DIR)
+	@$(MAKE) -C $(LIBFT) clean
+	@rm -rf $(LIBMLX)/build
+
+fclean: clean
+	@rm -rf $(NAME)
+	@$(MAKE) -C $(LIBFT) fclean
+
+re: fclean all
---
+++ source/main.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lodemetz <lodemetz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 16:52:15 by lodemetz          #+#    #+#             */
+/*   Updated: 2024/01/26 17:02:54 by lodemetz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	throw_error(t_env *env)
+{
+	ft_putstr_fd("Error\n", 1);
+	free_env(env);
+	exit(EXIT_FAILURE);
+}
+
+void	hook(void *param)
+{
+	t_env	*env;
+
+	env = param;
+	if (mlx_is_key_down(env->window, MLX_KEY_ESCAPE))
+		mlx_close_window(env->window);
+	if (mlx_is_key_down(env->window, MLX_KEY_UP))
+		translate_y(env, 20);
+	if (mlx_is_key_down(env->window, MLX_KEY_DOWN))
+		translate_y(env, -20);
+	if (mlx_is_key_down(env->window, MLX_KEY_RIGHT))
+		translate_x(env, -20);
+	if (mlx_is_key_down(env->window, MLX_KEY_LEFT))
+		translate_x(env, 20);
+	if (mlx_is_key_down(env->window, MLX_KEY_KP_ADD))
+		zoom(env, 0.1);
+	if (mlx_is_key_down(env->window, MLX_KEY_KP_SUBTRACT))
+		zoom(env, -0.1);
+	if (mlx_is_key_down(env->window, MLX_KEY_KP_8))
+		rotate(env, 0.01);
+	if (mlx_is_key_down(env->window, MLX_KEY_KP_2))
+		rotate(env, -0.01);
+}
+
+int	main(int argc, char **argv)
+{
+	mlx_t		*window;
+	mlx_image_t	*img;
+	t_env		*env;
+
+	if (argc == 2 && WIDTH > 50 && HEIGHT > 50)
+	{
+		window = mlx_init(WIDTH, HEIGHT, "fdf", false);
+		if (!window)
+			throw_error(0);
+		img = mlx_new_image(window, WIDTH, HEIGHT);
+		if (!img || (mlx_image_to_window(window, img, 0, 0) < 0))
+			throw_error(0);
+		env = init_env(window, img);
+		if (!env)
+			throw_error(env);
+		if (!load_map(env, argv[1]))
+			throw_error(env);
+		if (!plot_map(env))
+			throw_error(env);
+		mlx_loop_hook(window, hook, env);
+		mlx_loop(window);
+		free_env(env);
+	}
+	else
+		throw_error(0);
+	return (0);
+}
