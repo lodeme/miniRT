5eba3601ddff0f90037a9c461c15d27018e5d5e8
Urbano Jr <urbanobazz@gmail.com>
2024-05-10 13:30:48 +0200

    weird shading/camera behavior

--- source/intersections/hit_obj.c
+++ source/intersections/hit_obj.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   intersections.c                                    :+:      :+:    :+:   */
+/*   hit_obj.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lodemetz <lodemetz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:26:06 by lodemetz          #+#    #+#             */
-/*   Updated: 2024/05/02 17:54:02 by lodemetz         ###   ########.fr       */
+/*   Updated: 2024/05/10 13:29:36 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 /**
  * @brief Whether the ray casted has touched the sphere
- * 
- * @param center 
- * @param radius 
- * @param ray 
+ *
+ * @param center
+ * @param radius
+ * @param ray
  * @return if there is an intersection, return the t-value (length of
  * the ray from origin to intersection. Otherwise returns -1)
  */
-double	hit_sphere(t_vec center, double radius, t_ray *ray)
+/* double	hit_sphere(t_vec center, double radius, t_ray *ray)
 {
 	t_vec	oc;
 	double	a;
@@ -38,6 +38,37 @@ double	hit_sphere(t_vec center, double radius, t_ray *ray)
 		return (-1.0);
 	else
 		return ((-b - sqrt(discriminant)) / (2.0 * a));
+} */
+
+double	hit_sphere(t_vec center, double radius, t_ray *ray)
+{
+	t_vec	oc;
+	double	a;
+	double	b;
+	double	c;
+	double	discriminant;
+	double	t0;
+	double	t1;
+
+	oc = vec_sub(ray->origin, center);
+	a = vec_dot(ray->direction, ray->direction);
+	b = 2.0 * vec_dot(oc, ray->direction);
+	c = vec_dot(oc, oc) - radius * radius;
+	discriminant = b * b - 4 * a * c;
+	if (discriminant < 0)
+		return (-1.0);
+	else
+	{
+		t0 = (-b - sqrt(discriminant)) / (2.0 * a);
+		t1 = (-b + sqrt(discriminant)) / (2.0 * a);
+
+		if (t0 > 0)
+			return t0;
+		else if (t1 > 0)
+			return t1;
+		else
+			return (-1.0);
+	}
 }
 
 t_ray	create_ray(t_data *data, int x_index, int y_index)
